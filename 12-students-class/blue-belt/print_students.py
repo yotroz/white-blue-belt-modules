6107d3172ae46b3d0e8b6d9ae5205df383429038
Octavio <s.octavio90@gmail.comgit config --global user.email s.octavio90@gmail.comgit congfig --global user.name s.octavio90@gmail.comgit config --global user.name s.octavio90@gmail.com>
2018-11-19 20:34:11 +0100

    fixed: instructions

--- 12-students-class/blue-belt/print_students.py
+++ 12-students-class/blue-belt/print_students.py
@@ -1,2 +1,12 @@
-# -*- coding: utf-8 -*-
+#Add a print_student method in the Student class that 
+#prints a line like follows for the object
+#
+#
+#Pepe García is a 55 year old student of the MCSDBI masters programme
+#
+# 
+#Create a list of all 28 Students representing all students in this class.  
+#Then iterate over all of them and call 
+#print_student on each one to print its description.
+#%%
 
--- 12-students-class/white-belt/student_class.py
+++ 12-students-class/white-belt/student_class.py
@@ -1,2 +1,13 @@
-# -*- coding: utf-8 -*-
+#Create a Student class that has the following attributes:
+#
+#name
+#last name
+#age
+#master (either MCSBT or MDBI)
+# Make sure you parametrize all those fields in the constructor.
 
+
+#%%
+
+class Student: 
+    
