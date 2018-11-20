abb80b3c6f64bbda03c49aceacf52a07df04c7db
Octavio <s.octavio90@gmail.comgit config --global user.email s.octavio90@gmail.comgit congfig --global user.name s.octavio90@gmail.comgit config --global user.name s.octavio90@gmail.com>
2018-11-20 11:50:20 +0100

    incomplete attempt

--- 13-oop-audio-university-company-whatsapp/audio_player.py
+++ 13-oop-audio-university-company-whatsapp/audio_player.py
@@ -1,2 +1,40 @@
-# -*- coding: utf-8 -*-
+#%%
+class Song: 
+    
+    def __init__(self, name):
+        self.name = name
+        
+    def play(self): 
+        print("Now playing: " + self.name)
+        
+
+class Album: 
+    
+    songs = []
+    
+    def __init__(self, name):
+        self.name = name
+        
+    def add_song(self, song): 
+        self.songs.append(song)
+        
+    def add_songs_list(self, msongs_list): 
+        self.songs += songs_list
+        
+    def play(self): 
+        for song in self.songs:
+            
+            song.play()
+
+
+steel = Album("Steel")
+
+songs = [
+        Song("Red"),
+        Song("Rock"),
+        Song("Blue")
+        ]
+
+
+steel.add_songs_list(songs)
 
--- 13-oop-audio-university-company-whatsapp/university/university_correction.py
+++ 13-oop-audio-university-company-whatsapp/university/university_correction.py
@@ -1,2 +1,69 @@
-# -*- coding: utf-8 -*-
+#%%
 
+class Student: 
+    
+    def __init__(self, name, gpa): 
+        self.name = name
+        self.gpa = gpa 
+
+
+#We create a call-response between methods becase we want different
+#instances to respond differently.         
+    
+    def apply(self, university): 
+        if university.accepts(self): 
+            return True
+        else:
+            return False
+        
+
+
+class University: 
+    
+    def __init__(self, min_gpa, course_list): 
+        self.min_gpa = min_gpa
+        self.course_list = course_list
+        
+    def accepts(self, student): 
+        if student.gpa >= self.min_gpa: 
+            return True
+        else: 
+            return False 
+        
+class Course: 
+ 
+#Writting this and the self.students initialized to an empyt list, 
+#is the same. 
+    
+    students = []
+    
+    def __init__(self, name, max_students): 
+        self.name = name
+        self.max_students = max_students   
+#        self.students = []
+        
+    def enroll(self, student): 
+        if self.max_students > len(self.students): 
+            self.students.append(student)
+        else: 
+            print("bad luck fella")
+            
+            
+courses_delaware = [Course("tax evasion 101", 100)]
+delaware_uni = University(1.3, courses_delaware)
+pepe = Student("Pepe", 1.3)
+
+print(pepe.apply(delaware_uni))
+
+programming = Course("programming", 1)
+persuasion = Course("persuasion skills", 27)
+
+courses_ie = [
+        programming, 
+        persuasion
+        
+        ]
+
+ie_uni = University(3.6, courses_ie)
+
+print
