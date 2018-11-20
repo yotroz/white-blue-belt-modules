f11c7eee8fb7b7625350d584e57d3bb1fd2b7db6
Octavio <s.octavio90@gmail.comgit config --global user.email s.octavio90@gmail.comgit congfig --global user.name s.octavio90@gmail.comgit config --global user.name s.octavio90@gmail.com>
2018-11-20 08:40:54 +0100

    white belt done

--- 12-students-class/black-belt/test_music-store-oop-reloaded.py
+++ 12-students-class/black-belt/test_music-store-oop-reloaded.py
@@ -1,15 +1,4 @@
-#Migrate the music store program we created to an OOP approach.  
-#You can use classes for modelling products, services, 
-#customer tier, the shopping cart...
-#
-#Products have name and price.
-#Services have price.
-#Customer tier have a discount.
-#Shopping cart has:
-#a list of products
-#a list of services
-#and a method to add new products and another for services
-#a checkout method that receives a customer tier and calculates the price
+
 
 #%%
 
--- 12-students-class/white-belt/student_class.py
+++ 12-students-class/white-belt/student_class.py
@@ -10,4 +10,11 @@
 #%%
 
 class Student: 
+    def __init__(self, name, last_name, age, master): 
+        self.name = name
+        self.last_name = last_name
+        self.age = age
+        self.master = master
+
+tancredi = Student("Tancredi", "Rossetti", "9", "MCSBT")
     
