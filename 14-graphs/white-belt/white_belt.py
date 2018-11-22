60aeec80ce0eb0c11dd23207512257070b9e8562
Octavio <s.octavio90@gmail.comgit config --global user.email s.octavio90@gmail.comgit congfig --global user.name s.octavio90@gmail.comgit config --global user.name s.octavio90@gmail.com>
2018-11-22 19:32:19 +0100

    white belt cleaner, works. No test suite yet

--- 14-graphs/white-belt/white_belt.py
+++ 14-graphs/white-belt/white_belt.py
@@ -2,140 +2,47 @@
 
 #Create a function non_connected to find non-connected nodes in a graph
 
-graph =  {
-            "a": ["b","c"],
-            "b": ["d"],
-            "c" : ["d"],
-            "d" : ['e'],
-            "e": [], 
-            "f": []
-            }
-
-def non_connected(g): 
-    a = []
-#    
-#    for node in g: 
-#        for edge in g[node]:
-#           print(len(g[node]))
-#           if len(g[node]) == a: 
-#               print(node)
-##                print(len(g[node])
-##    
-#    i = 0 
-#    
-#    for node in g: 
-#        if node[i] == a: 
-#            print("yes")
-#            i+=1
-    
-    for node in g: 
-        if g[node] == a: 
-            return "the node '" + node + "' has no edges "
-
-#    for node in g: 
-#        for edge[g] in 
-#        if node.value() == a:
-#            return node.item()
-            
-        
-def non_connected_reloaded(g): 
-    
-    a = []
-    b =[]
-    i = 0
-    y= 0
-    
-    nodes_without_edges = []
-    
-    for node in g: 
-        if g[node] == b:
-            a.append(node)
-            print(a)
-            i +=1
-#     
-
-    for node in g: 
-#        print(g[node])
-        if a[y] in g[node]: 
-#           return 
-            print(g[node][y]  + " is in " +  node)
-            y+=1
-        else:
-            print(g[node][y]  + " is not in " +  node)
-            nodes_without_edges.append(node)
-            y+=1
-        
-    return nodes_without_edges
-            
-#            return a[0] + " Has an edge with " + node 
-        
-        
-#    for node2 in g: 
-#        print(g[node2])
-#        if a[0] not in g[node2]: 
-#            return "the node '" + a[0] + "' has no edges "
-#                
-#        else: 
-#            return "they all have edges"
-#            
-non_connected_reloaded(graph)
 
 #%%
 
 
-graph =  {
+graph1 =  {
             "a": ["b","c"],
             "b": ["d"],
             "c" : ["d"],
             "d" : ['e'],
             "e": [], 
-            "f": []
+            "f": [], 
+            "g":[]
             }
 
-def non_connected_try(g): 
-    a = []
-    b =[]
-    i = 0
-    y= 0
+def non_connected_try(graph): 
     
+#    we declare empty lists that we will use to compare later on. 
+    
+    nodes_with_empty_values = []
     nodes_without_edges = []
     
-    for node in g: 
-        if g[node] == b:
-            a.append(node)
-            print(a)
-            i +=1
-            
-    print("this are my nodes without edges" + str(nodes_without_edges))        
+#    we find all the nodes that comply the first condition: not having a
+#    reference in their own values. 
     
-    for node in a: 
-        print(node)
+    for node in graph: 
+        if graph[node] == []:
+           nodes_with_empty_values.append(node)
+    
+#    we compare the nodes with empty values with the values of all of them to 
+#    see if there is a value in other nodes. 
+    
+    for node in nodes_with_empty_values: 
         counterno = 0 
-        for other_node in g:
-            if node not in g[other_node]:
+        for other_node in graph:
+            if node not in graph[other_node]:
                 counterno += 1
-                print(counterno)
-                if counterno == len(g): 
-                    return node + " NOT ANYWHERE"
-                
-                    print(node + " is a reference somewhere")
-#                break
-#                print("not anywhere")
-                
-                
-                    
-#            print(other_node)
-#            if node in g[other_node]:
-#                print(node + "this one isnt in " + other_node)
-#                  
-#            elif: 
-#            else:   
-#                nodes_without_edges.append(node)
-#                pass
-#                print("this are my nodes without edges" + str(nodes_without_edges)) 
-                
-    
-#    return nodes_without_edges
+                if counterno == len(graph): 
+                    nodes_without_edges.append(node)
+                    print(node + " has no edges")
+
+    return "This nodes have no edges: " + str(nodes_without_edges)
     
     
     
