05a90639c10299d8813987ad3def86fe499410e4
Octavio <s.octavio90@gmail.comgit config --global user.email s.octavio90@gmail.comgit congfig --global user.name s.octavio90@gmail.comgit config --global user.name s.octavio90@gmail.com>
2018-11-23 11:24:25 +0100

    improved, first round of tests passed

--- 14-graphs/blue-belt/blue_belt.py
+++ 14-graphs/blue-belt/blue_belt.py
@@ -1,4 +1,52 @@
 #%%
+#%%
+from itertools import combinations
+
+graph10 =  {
+            "a": ["b","c"],
+            "b": ["c"],
+            "c" : ["b", "d"],
+            "d" : ["b"],
+            }
+
+graph2 = {
+        "a": ["b", "c", "d"], 
+        "b": ["c"], 
+        "c": [], 
+        "d": ["c"]
+               }
+        
+graph3 = {
+        "a": [],
+        "b": ["c", "d", "a"], 
+        "c": ["d", "a"], 
+        "d": ["a"]
+        }        
+        
+ 
+
+def fully_connected(graph):  
+       
+    nodes = [nodes for nodes in graph]    
+    
+    combinations_list = list(combinations(nodes,2))
+    
+    combinations_clean = [''.join(element) for element in combinations_list]
+      
+    edges = [node + other_node for node in graph for other_node in graph[node]]   
+    
+    counter = 0
+    for combination in combinations_clean: 
+        if combination in edges or combination[::-1] in edges: 
+            counter += 1
+            if counter == len(combinations_clean): 
+                return "THE GRAPH IS TOTALLY FOOOKING CONNECTED"
+
+    return "FOOOK NO!! THE GRAPH IS NOT TOTALLY CONNECTED"
+                            
+
+
+#%%
 from itertools import combinations
 
 graph10 =  {
--- 14-graphs/blue-belt/test_blue_belt.py
+++ 14-graphs/blue-belt/test_blue_belt.py
@@ -1,8 +1,30 @@
-from itertools import combinations
+#%%
 
 from blue_belt import fully_connected
 
-
+def test_six_but_one_missing_connection(): 
+    
+    value = [
+                {
+                "a": ["b","c"],
+                "b": ["c"],
+                "c": ["b", "d"],
+                "d": ["b"],
+                }, 
+                        
+                {
+                "a": ["d"],
+                "b": ["c", "d", "a"],
+                "c": ["a"],
+                "d": ["a"],
+                },                         
+            
+                
+            ]
+    
+    for case in value:
+        
+        assert fully_connected(case) == "FOOOK NO!! THE GRAPH IS NOT TOTALLY CONNECTED"
 
 
 
