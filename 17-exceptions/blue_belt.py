00bcbed954bf6cb79b169d57c5e5f6bb303a38fe
Octavio <s.octavio90@gmail.comgit config --global user.email s.octavio90@gmail.comgit congfig --global user.name s.octavio90@gmail.comgit config --global user.name s.octavio90@gmail.com>
2018-12-01 09:39:07 +0100

    done

--- 17-exceptions/blue_belt.py
+++ 17-exceptions/blue_belt.py
@@ -1,3 +1,24 @@
 #%%
+#Create a function that reads through a file 
+#and prints all the lines in uppercase.
+#
+# 
+#
+#be sure to control exceptions that may occur here, 
+#such as the file not existing
+
+
 
 def print_file_uppercase(filename): 
+    
+    try: 
+    
+        file = open(filename)
+        for line in file: 
+            print(line.upper().strip())
+    except Exception: 
+        print("file doesnt exist")
+        
+    
+print_file_uppercase("data.txt")
+print_file_uppercase("other_file.txt")
